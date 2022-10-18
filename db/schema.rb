4587480b6e628deb67107b03eccc11bca5b4f37f
timamuravyov <timamuravyov@timamuravyovs-MacBook-Pro.local>
2022-10-18 19:15:17 +0300

    feat(crud): add crud operations for expenses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_123132) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_104617) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,7 +21,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_123132) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.string "category"
     t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
